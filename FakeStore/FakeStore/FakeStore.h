d1b6c6440a55fc9607416ca9c7293723329a5577
Husam Dayya <husamdayya@icloud.com>
2023-01-12 20:10:51 +0200

    Added `FakeStore`

---
+++ FakeStore/FakeStore/FakeStore.h
@@ -0,0 +1,18 @@
+//
+//  FakeStore.h
+//  FakeStore
+//
+//  Created by Husam Dayya on 09/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FakeStore.
+FOUNDATION_EXPORT double FakeStoreVersionNumber;
+
+//! Project version string for FakeStore.
+FOUNDATION_EXPORT const unsigned char FakeStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FakeStore/PublicHeader.h>
+
+
---
+++ FakeStore/FakeStoreTests/FakeStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreTests.swift
+//  FakeStoreTests
+//
+//  Created by Husam Dayya on 09/01/2023.
+//
+
+import XCTest
+@testable import FakeStore
+
+final class FakeStoreTests: XCTestCase {
+
+    func test() {
+        
+    }
+
+}
