ec0d7f91326930cd36d6a62a861c8138d070476d
Husam Dayya <husamdayya@icloud.com>
2023-02-01 14:22:33 +0200

    Make http client a protocol

--- FakeStore/FakeStore/RemoteProductsLoaderTests.swift
+++ FakeStore/FakeStore/RemoteProductsLoaderTests.swift
@@ -8,22 +8,27 @@
 import XCTest
 
 class RemoteProductsLoader {
+    
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load() {
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
-class HTTPClient {
-    static var shared = HTTPClient()
-            
-    func get(from url: URL) {}
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
     
     var requestedURL: URL?
     
-    override func get(from url: URL) {
+    func get(from url: URL) {
         requestedURL = url
     }
 }
@@ -32,8 +37,7 @@ final class RemoteProductsLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        _ = RemoteProductsLoader()
+        _ = RemoteProductsLoader(client: client)
         
         
         XCTAssertNil(client.requestedURL)
@@ -41,8 +45,7 @@ final class RemoteProductsLoaderTests: XCTestCase {
     
     func test_load_requestsDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteProductsLoader()
+        let sut = RemoteProductsLoader(client: client)
         
         sut.load( )
         
