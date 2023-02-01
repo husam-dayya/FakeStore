eec97357f5b9964db0712f3eead13c275889616e
Husam Dayya <husamdayya@icloud.com>
2023-02-01 14:16:28 +0200

    Requested data from url on load

--- FakeStore/FakeStore/RemoteProductsLoaderTests.swift
+++ FakeStore/FakeStore/RemoteProductsLoaderTests.swift
@@ -8,21 +8,36 @@
 import XCTest
 
 class RemoteProductsLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 final class RemoteProductsLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteProductsLoader()
         
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteProductsLoader()
+        
+        sut.load( )
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
