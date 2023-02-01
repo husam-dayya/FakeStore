cf2fa47c2b8f35dc24ed95f151d46ab13ed61e6f
Husam Dayya <husamdayya@icloud.com>
2023-02-01 14:05:13 +0200

    Created product model

---
+++ FakeStore/FakeStore/Product.swift
@@ -0,0 +1,17 @@
+//
+//  Product.swift
+//  FakeStore
+//
+//  Created by Husam Dayya on 01/02/2023.
+//
+
+import Foundation
+
+struct Product {
+    let id: Int
+    let title: String
+    let price: Double
+    let description: String?
+    let category: String
+    let image: URL
+}
