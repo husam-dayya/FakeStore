bacd6c2806f6151845d48fde75da10e0d725997e
Husam Dayya <husamdayya@icloud.com>
2023-02-08 14:04:26 +0200

    Give a URL instead of hardcoding it in the remote products loader

--- FakeStore/FakeStoreTests/RemoteProductsLoaderTests.swift
+++ FakeStore/FakeStoreTests/RemoteProductsLoaderTests.swift
@@ -9,14 +9,16 @@ import XCTest
 
 class RemoteProductsLoader {
     
+    private let url: URL
     private let client: HTTPClient
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -27,20 +29,22 @@ protocol HTTPClient {
 final class RemoteProductsLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteProductsLoader(client: client)
+        _ = RemoteProductsLoader(url: url, client: client)
         
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteProductsLoader(client: client)
+        let sut = RemoteProductsLoader(url: url, client: client)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
     
     // MARK: - Helpers
